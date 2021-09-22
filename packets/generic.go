ac81fd2e0fe039d47a9c137de193ac999dfc7f2b
Spencer Ragen <sragen@gmail.com>
2021-09-21 20:01:51 -0400

    streamlined the GetBytes error messaging a bit

--- packets/generic.go
+++ packets/generic.go
@@ -30,6 +30,7 @@ func (d BNCSBase) String() string {
 // Technically works on basically anything, but mostly useful for converting packets between
 // BNCSGeneric and a specific struct. Also for dumping to logs
 func GetBytes(d interface{}) []byte {
+	var err error
 	buf := new(bytes.Buffer)
 
 	dv := reflect.ValueOf(d)
@@ -40,40 +41,34 @@ func GetBytes(d interface{}) []byte {
 		switch dv.Field(i).Kind() {
 		case reflect.Slice:
 			slice_data := reflect.ValueOf(dv.Field(i).Interface()).Interface()
-			err := binary.Write(buf, binary.LittleEndian, slice_data)
-			if err != nil {
-				log.Println("binary.Write failed:", err)
-			}
+			err = binary.Write(buf, binary.LittleEndian, slice_data)
 		case reflect.Struct:
 			struct_data := GetBytes(dv.Field(i).Interface())
-			err := binary.Write(buf, binary.LittleEndian, struct_data)
-			if err != nil {
-				log.Println("binary.Write failed:", err)
-			}
+			err = binary.Write(buf, binary.LittleEndian, struct_data)
 		case reflect.String:
 			var buf2 bytes.Buffer
 			enc := gob.NewEncoder(&buf2)
-			err := enc.Encode(dv.Field(i).Interface())
+			err = enc.Encode(dv.Field(i).Interface())
 			if err != nil {
 				log.Println("failed to encode string as bytes")
+				continue
 			}
 
 			bs := buf2.Bytes()
 			if bs[len(bs)-1] != 0x00 {
+				// make sure the string is null terminated
 				bs = append(bs, 0x00)
 			}
 			err = binary.Write(buf, binary.LittleEndian, bs)
-			if err != nil {
-				log.Println("binary.Write failed:", err)
-			}
 		case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			err := binary.Write(buf, binary.LittleEndian, dv.Field(i).Interface())
-			if err != nil {
-				log.Println("binary.Write failed:", err)
-			}
+			err = binary.Write(buf, binary.LittleEndian, dv.Field(i).Interface())
 
 		default:
 			log.Println("failed to convert field: ", dv.Type().Field(i).Name, dv.Field(i).Kind())
+		}
+		if err != nil {
+			log.Println("binary.Write failed:", err)
+			err = nil
 		}
 	}
 
